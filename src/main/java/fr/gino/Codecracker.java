11d21b05bef3edd5e68f4bfa97a5d212b5374c12
GinoChasles <gino.chasles@gmail.com>
2020-11-07 21:48:49 +0100

    dernière modif

--- src/main/java/fr/gino/Codecracker.java
+++ src/main/java/fr/gino/Codecracker.java
@@ -2,30 +2,19 @@ package fr.gino;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Codecracker {
 
-    public static void main(String[] args) {}
-
-    public static String[] letterIndexOfWord(String word) {
-        String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",};
-        String[] defaultKey = {"!", ")","\"","(","£","*","%","&",">","<","@","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"};
-
-        List<String> alphabetList = Arrays.asList(alphabet);
-
-        String[] letterOfWord = word.split("");
-        int[] positionOfLetter = new int[letterOfWord.length];
-
-        for (int i = 0; i < letterOfWord.length; i++) {
-            positionOfLetter[i] = alphabetList.indexOf(letterOfWord[i]);
-        }
-
-        String[] codedWord = new String[letterOfWord.length];
-
-        for (int i = 0; i < letterOfWord.length; i++) {
-            codedWord[i] = defaultKey[positionOfLetter[i]];
-        }
-        return codedWord;
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Saisissez votre clé de cryptage. Taper entrée pour prendre la clé par défaut. ");
+        String key = scan.nextLine();
+        chooseYourKey(key);
+        System.out.println("Mot a coder: ");
+        String motACoder = scan.nextLine();
+        String motCoder = codeYourWord(motACoder, key);
+        System.out.println(motCoder);
     }
 
     public static String[] chooseYourKey(String key){
@@ -43,40 +32,32 @@ public class Codecracker {
             return finalKey;
         }
 
-        public static String codeYourWord(String wordForConvert, String key){
-            chooseYourKey(key);
-            return letterIndexOfWord(wordForConvert).toString();
-
-        }
-
-
-
         public static String letterIndexOfWord2(String word, String key) {
             String[] alphabet = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",};
             String[] keyUse = chooseYourKey(key);
             List<String> alphabetList = Arrays.asList(alphabet);
             String wordConverted = "";
-    
+
             String[] letterOfWord = word.split("");
             int[] positionOfLetter = new int[letterOfWord.length];
-    
+
             for (int i = 0; i < letterOfWord.length; i++) {
                 positionOfLetter[i] = alphabetList.indexOf(letterOfWord[i]);
             }
-    
+
             String[] codedWord = new String[letterOfWord.length];
-    
+
             for (int i = 0; i < letterOfWord.length; i++) {
                 codedWord[i] = keyUse[positionOfLetter[i]];
             }
-
             for(int j=0; j<codedWord.length; j++){
                 wordConverted += codedWord[j] + "";
-            }            
-            // wordConverted = Arrays.asList(codedWord).toString();
-            // return Arrays.toString(codedWord);
+            }
             return wordConverted;
         }
-    
+        public static String codeYourWord(String wordForConvert, String key){
+            chooseYourKey(key);
+            return letterIndexOfWord2(wordForConvert, key).toString();
+        }
     }
 
--- src/test/java/fr/gino/CodecrackerTest.java
+++ src/test/java/fr/gino/CodecrackerTest.java
@@ -8,50 +8,43 @@ import org.junit.jupiter.api.Test;
 public class CodecrackerTest {
 
     @Test
-    public void letterIndexOfWord() {
-        String[] indexWord = { "%", ">", "c", "d" };        
-        String[] codedWord2 = { "*", "g", "!", "c", "\"", "@" };
-        assertArrayEquals(indexWord, Codecracker.letterIndexOfWord("gino"));
-        assertArrayEquals(codedWord2, Codecracker.letterIndexOfWord("franck"));
-    }
-    
-    @Test
     public void chooseYourKeyTest(){
         String[] defaultKey = {"!", ")","\"","(","£","*","%","&",">","<","@","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"};
         String[] arrayExpected= {"f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r"};
         assertArrayEquals(arrayExpected, Codecracker.chooseYourKey("franck"));
         assertArrayEquals(defaultKey,Codecracker.chooseYourKey(""));
     }
-    
+
     @Test
     public void codeYourWordTest(){
-        
+        assertEquals("%>cd", Codecracker.codeYourWord("gino", ""));
+        assertEquals("*g!c\"@", Codecracker.letterIndexOfWord2("franck", ""));
         assertEquals("kkfrac", Codecracker.codeYourWord("franck", "franck"));
         assertEquals("fara", Codecracker.codeYourWord("gino", "franck"));
-        assertEquals("igginn", Codecracker.codeYourWord("franck", "gino"));
+        assertEquals("iiginn", Codecracker.codeYourWord("franck", "gino"));
         assertEquals("ngin", Codecracker.codeYourWord("gino", "gino"));
     }
     @Test
     public void letterIndexOfWord2(){
-        String[] indexWord = { "%", ">", "c", "d" };
-        assertArrayEquals(indexWord, Codecracker.letterIndexOfWord("gino"));
+        assertEquals("%>cd", Codecracker.letterIndexOfWord2("gino", ""));
+        assertEquals("*g!c\"@", Codecracker.letterIndexOfWord2("franck", ""));
         assertEquals("kkfrac", Codecracker.letterIndexOfWord2("franck", "franck"));
         assertEquals("fara", Codecracker.letterIndexOfWord2("gino", "franck"));
         assertEquals("iiginn", Codecracker.letterIndexOfWord2("franck", "gino"));
         assertEquals("ngin", Codecracker.letterIndexOfWord2("gino", "gino"));
 
+
     }
 }
 
-// String[] arrayExpected= {"f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r", "a", "n", "c", "k","f", "r"};
 // a b c d e f g h i j k l m n o p q r s t u v w x y z
-// f r a n c k f r a n c k f r a n c k f r a n c k f r 
-
+// ! ) " ( £ * % & > < @ a b c d e f g h i j k l m n o
+//  franck donne * g ! c  " @
 // franck donne k k f r a c
 //  gino donne f a r a
 
 // a b c d e f g h i j k l m n o p q r s t u v w x y z
-// g i n o g i n o g i n o g i n o g i n o g i n o g i n
+// g i n o g i n o g i n o g i n o g i n o g i n o g i
 
 // franck donne i i g i n n
 // gino donne n g i n
